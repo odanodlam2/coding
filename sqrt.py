ddd44698f0edad94c3dc10f4468b4f1cb46e3568
pirateman2 <156225167+odanodlam2@users.noreply.github.com>
2024-01-24 12:51:49 -0500

    Update sqrt.py

--- sqrt.py
+++ sqrt.py
@@ -1,23 +1,29 @@
 import math
 
-def sqrt(num):
+def BabylonianAlgorithm(number):
+    if number == 0: return 0
+    
     guess = 1
-    last = -1
-    while guess<=num:
-        guess+=1
-        guess*=guess
-    root = guess
+    i = 1
+    
+    print("\nProcess to find the initial guess:")
     while True:
-        root=(root + num/root)/2
-        if last == root:
+        guess = i**2
+        if guess > number: 
             break
         else: 
-            last = root
+            print(f'Guess: {i} = {guess}')
+            i += 1
+    i -= 1
+    print(f"\n The Guess Number is: {i}, because {i}*{i} = {i*i} which is less than: {number} \n")
+    root = i
+    for i in range(10):
+        print(f"interation {i}: {root}")
+        root = (root + number/root)*0.5
 
     return root
 
-
-number = float(input("input number to sqrt"))
-
-print("my method: " + str(sqrt(number)))
-print("internal function " + str(math.sqrt(number)))
+number = int(input("Enter a number to get its square root: "))
+result = BabylonianAlgorithm(number)
+print(f"\nThe square root with this Babylonian method is: {result}")
+print(f"The square root using the internal function math.sqrt() is: {math.sqrt(number)}")
