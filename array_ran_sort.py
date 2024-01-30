d2524e97c97fe2dc6bb9854e460dc54893692814
pirateman2 <156225167+odanodlam2@users.noreply.github.com>
2024-01-30 10:16:05 -0500

    Create array_ran_sort.py
    !!!!!nOT DONE WORK IN PROGRESS

---
+++ array_ran_sort.py
@@ -0,0 +1,16 @@
+!!!!!nOT DONE WORK IN PROGRESS
+!!!!!nOT DONE WORK IN PROGRESS
+!!!!!nOT DONE WORK IN PROGRESS
+!!!!!nOT DONE WORK IN PROGRESS
+
+import random
+
+array = [random.randint(1,10) for i in range(10)]
+array.sort()
+
+print(array)
+!!!!!nOT DONE WORK IN !!!!!nOT DONE WORK IN PROGRESS
+
+!!!!!nOT DONE WORK IN PROGRESS
+
+!!!!!nOT DONE WORK IN PROGRESS
