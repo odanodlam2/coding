b547ecb015ef1491166592820ae12c1976b22d26
pirateman2 <156225167+odanodlam2@users.noreply.github.com>
2024-01-30 12:08:32 -0500

    Update array_ran_sort.py

--- array_ran_sort.py
+++ array_ran_sort.py
@@ -4,6 +4,6 @@ list_random = [random.randint(1,100) for i in range(20)]
 list_ascending = sorted(list_random)
 list_descending = sorted(list_random, reverse=True)
 
-print(f"Original: {list_random}")
-print("Ascending list: {list_ascending}")
+print(f"Original: {list_random} \n")
+print("Ascending list: {list_ascending} \n")
 print("Descending list: {list_descending}")
