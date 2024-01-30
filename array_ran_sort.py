bb921c8eedde18b0d6d92e65bb527f596f3ead55
pirateman2 <156225167+odanodlam2@users.noreply.github.com>
2024-01-30 17:25:00 -0500

    Update array_ran_sort.py

--- array_ran_sort.py
+++ array_ran_sort.py
@@ -1,9 +1,20 @@
 import random
 
+def bubblesort(elements):
+    swapped = False
+    for n in range(len(elements)-1, 0, -1):
+        for i in range(n):
+            if elements[i] > elements[i + 1]:
+                swapped = True
+                elements[i], elements[i + 1] = elements[i + 1], elements[i]        
+        if not swapped:
+            return
+    return elements
+
 list_random = [random.randint(1,100) for i in range(20)]
-list_ascending = sorted(list_random)
-list_descending = sorted(list_random, reverse=True)
+list_ascending = bubblesort(list_random)
+list_descending = list(reversed(list_ascending))
 
 print(f"Original: {list_random} \n")
-print("Ascending list: {list_ascending} \n")
-print("Descending list: {list_descending}")
+print(f"Ascending list: {list_ascending} \n")
+print(f"Descending list: {list_descending}")
