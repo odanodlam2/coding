4b591b1211e1846cd503c3805a03327f48d02a89
pirateman2 <156225167+odanodlam2@users.noreply.github.com>
2024-02-02 08:00:18 -0500

    Update array_ran_sort.py

--- array_ran_sort.py
+++ array_ran_sort.py
@@ -1,14 +1,10 @@
 import random
 
 def bubblesort(elements):
-    swapped = False
     for n in range(len(elements)-1, 0, -1):
         for i in range(n):
             if elements[i] > elements[i + 1]:
-                swapped = True
                 elements[i], elements[i + 1] = elements[i + 1], elements[i]        
-        if not swapped:
-            return
     return elements
 
 list_random = [random.randint(1,100) for i in range(20)]
